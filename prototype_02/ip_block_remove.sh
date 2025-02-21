603ab5a63123443c479ff8f82507a66f31e5e0dd
Sei <hazen.60.t@gmail.com>
2025-02-21 13:20:41 +0100

    Added chain remover script

---
+++ prototype_02/ip_block_remove.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Delete the chain on iptables
+echo "Iptables chain deletion has started..."
+
+iptables -D INPUT -j DROP-BLOCKED > /dev/null 2>&1
+iptables -F DROP-BLOCKED > /dev/null 2>&1
+iptables -X DROP-BLOCKED > /dev/null 2>&1
+
+# Show currently iptables list
+iptables -nvxL
+
+echo "iptables chain deletion complete."
